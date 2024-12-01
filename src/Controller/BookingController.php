2f5d9ebb0e4077c2baeee12eaf455784b8440b37
Ahmed191223 <ahmed.yakoubi@yess-consulting.com>
2024-12-01 14:06:02 +0100

    first

---
+++ src/Controller/BookingController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BookingController extends AbstractController
+{
+    #[Route('/', name: 'ma_page')]
+    public function index(): Response
+    {
+        return $this->render('index.html.twig');
+    }
+}
