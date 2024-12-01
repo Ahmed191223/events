0cec66156bb6d95e6340beea570e2a176219de44
haythem kouka <kouka.haythem@yess-consulting.com>
2024-12-01 14:23:33 +0100

    User entity

---
+++ src/Entity/User.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: "App\Repository\UserRepository")]
+#[ORM\Table(name: "user")]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: "integer")]
+    private ?int $id = null;
+
+    #[ORM\Column(type: "string", length: 255, unique: true)]
+    private string $email;
+
+    #[ORM\Column(type: "json")]
+    private array $roles = [];
+
+    #[ORM\Column(type: "string", length: 255)]
+    private string $password;
+
+    #[ORM\Column(type: "string", length: 255)]
+    private string $name;
+
+    // Getter et Setter pour l'id
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    // Getter et Setter pour l'email
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    // Getter et Setter pour les rôles
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        $roles[] = 'ROLE_USER';
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+        return $this;
+    }
+
+    // Getter et Setter pour le mot de passe
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    // Getter et Setter pour le nom
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+}
